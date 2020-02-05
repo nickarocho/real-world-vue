835f9c054a7357d894a2c811191746e9bcd97732
Nicholas.Arocho <nick@boldlyfine.com>
2020-02-04 16:05:24 -0800

    Complete routing lesson

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,39 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventCreate from '@/views/EventCreate.vue'
+import EventList from '@/views/EventList.vue'
+import EventShow from '@/views/EventShow.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
-const routes = [
+const routes = [{
+    path: '/',
+    name: 'event-list',
+    component: EventList
+  },
+  // {
+  //   path: '/about-us',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import( /* webpackChunkName: "about" */ '../views/About.vue')
+  // },
   {
-    path: "/",
-    name: "home",
-    component: Home
+    path: '/event',
+    name: 'event-show',
+    component: EventShow
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate
   }
-];
+]
 
 const router = new VueRouter({
   routes
-});
+})
 
-export default router;
+export default router
