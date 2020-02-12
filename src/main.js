0eaca7fd0ca9bdaea992f0e3c37b93ef8a9d5115
Nicholas.Arocho <nick@boldlyfine.com>
2020-02-12 15:27:18 -0800

    Create global component, nest other components, add global styles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,30 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import upperFirst from 'lodash/upperFirst'
+  import camelCase from 'lodash/camelCase'
+  
+  const requireComponent = require.context( // WebPack function, takes three params
+    './components', // 1) directory to search within
+    false, // 2) whether to search sub-directories
+    /Base[A-Z]\w+\.(vue|js)$/ // 3) regular expression
+  )
+  
+  requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = upperFirst(
+    camelCase(
+      fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
+    )
+  )
+  
+  Vue.component(
+    componentName,
+    componentConfig.default || componentConfig // depending on how the component exports itself
+  )
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
