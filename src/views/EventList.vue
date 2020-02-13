7bf17196e218c0246ad733aaa49c23175a3b1231
Nicholas.Arocho <nick@boldlyfine.com>
2020-02-13 08:26:57 -0800

    Convert event list to dynamically render via API

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard/>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
     <BaseIcon/>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue';
+import axios from 'axios';
+
 export default {
   components: {
     EventCard,
   },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events')
+      .then(response => {
+        this.events = response.data // updates the internal data()
+      })
+      .catch(error => {
+        console.log(`There was an error: ${error.response}`);
+      })
+  }
 }
 </script>
 
